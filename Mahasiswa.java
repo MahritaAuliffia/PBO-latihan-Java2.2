12e36ff457dd523b17eaaf32aa4756028d4f0adc
Mahrita Auliffia <81166544+MahritaAuliffia@users.noreply.github.com>
2021-04-04 21:06:49 +0800

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+  public static void tampilaninfo(){
+    System.out.println();
+    System.out.println("== Data Mahasiswa ===================");
+    System.out.println("NPM  : 19710149");
+    System.out.println("Nama : Mahrita Auliffia");
+    System.out.println("=====================================");  
+  }  
+}
